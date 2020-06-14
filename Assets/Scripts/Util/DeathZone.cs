1affdc0d66c3fe6222a1462fc5fc9362f68ac550
John  Beinecke <john.beinecke@mni.thm.de>
2020-06-14 12:02:53 +0200

    Changed plane size and implemented death zone

---
+++ Assets/Scripts/Util/DeathZone.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+	private void OnTriggerEnter(Collider other)
+	{
+		Unit unitHit;
+		if ((unitHit = other.GetComponent<Unit>()) != null && unitHit.isLocalPlayer)
+		{
+			unitHit.CmdChangeHealth(-100000);
+		}
+	}
+}
